d3edb3a30955b3e3993619fdb2760dc044c8b26a
Oleg Grenrus <oleg.grenrus@iki.fi>
2017-11-21 03:44:34 +0200

    Use inspection-testing-0.1.2

--- fin/test/Inspection.hs
+++ fin/test/Inspection.hs
@@ -1,14 +1,15 @@
+{-# LANGUAGE DeriveGeneric   #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# OPTIONS_GHC -O -fplugin Test.Inspection.Plugin #-}
 module Main (main) where
 
 import Data.Tagged     (Tagged (..), retag)
+import GHC.Generics    (Generic)
 import Test.Inspection
 
+import qualified Data.Fin      as F
+import qualified Data.Fin.Enum as E
 import qualified Data.Type.Nat as N
-
--- import qualified Data.Fin as F
--- import qualified Data.Fin.Enum as E
 
 -------------------------------------------------------------------------------
 -- InlineInduction
@@ -32,19 +33,21 @@ inspect $ 'lhsNormal =/= 'rhs
 -- Enum
 -------------------------------------------------------------------------------
 
-{-
--- inspection testing isn't smart enough here.
+-- | Note: GHC 8.0 (but not GHC 8.2?) seems to be
+-- so smart, it reuses dictionary value.
+--
+-- Therefore, we define own local Ordering'
+data Ordering' = LT' | EQ' | GT' deriving (Generic)
 
-lhsEnum :: Ordering -> F.Fin N.Nat3
+lhsEnum :: Ordering' -> F.Fin N.Nat3
 lhsEnum = E.gfrom
 
-rhsEnum :: Ordering -> F.Fin N.Nat3
-rhsEnum LT = F.Z
-rhsEnum EQ = F.S F.Z
-rhsEnum GT = F.S (F.S F.Z)
+rhsEnum :: Ordering' -> F.Fin N.Nat3
+rhsEnum LT' = F.Z
+rhsEnum EQ' = F.S F.Z
+rhsEnum GT' = F.S (F.S F.Z)
 
 inspect $  'lhsEnum ==- 'rhsEnum
--}
 
 -------------------------------------------------------------------------------
 -- Main to make GHC happy
