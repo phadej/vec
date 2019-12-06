648a0c52b3780eb4c1463f6f9b8f088519015f8c
Oleg Grenrus <oleg.grenrus@iki.fi>
2019-12-06 20:55:31 +0200

    Simpler pop definition

--- bin/src/Data/Bin/Pos.hs
+++ bin/src/Data/Bin/Pos.hs
@@ -12,7 +12,7 @@
 module Data.Bin.Pos (
     Pos (..), PosP,
     -- * Top & Pop
-    top, pop, Pop,
+    top, pop,
     -- * Showing
     explicitShow,
     explicitShowsPrec,
@@ -32,14 +32,13 @@ import Prelude
        fmap, fromIntegral, map, showParen, showString, ($), (.))
 
 import Data.Bin        (Bin (..), BinP (..))
-import Data.BinP.PosP  (PosP (..), PosP' (..))
-import Data.Nat        (Nat (..))
+import Data.BinP.PosP  (PosP (..))
 import Data.Typeable   (Typeable)
-import Data.Wrd        (Wrd (..))
 import Numeric.Natural (Natural)
 
 import qualified Data.BinP.PosP  as PP
 import qualified Data.Type.Bin   as B
+import qualified Data.Type.BinP   as BP
 import qualified Test.QuickCheck as QC
 
 import Data.Type.Bin
@@ -156,38 +155,8 @@ top :: SBinPI b => Pos ('BP b)
 top = minBound
 
 -- | See 'top'.
-pop :: Pop a b => Pos ('BP a) -> Pos ('BP b)
-pop (Pos (PosP n)) = Pos (PosP (pop' n))
-
--- | Class implmenenting 'pop'.
-class Pop a b | b -> a where
-    pop' :: PosP' n a -> PosP' n b
-
-instance SBinPI b => Pop ('B0 b) ('B1 b) where
-    pop' = PP.weakenRight1' sbinp
-
-instance Pop0 a b => Pop a ('B0 b) where
-    pop' = pop0
-
-class Pop0 a b | b -> a where
-    pop0 :: PosP' n a -> PosP' n ('B0 b)
-
-instance Pop0 'BE 'BE where
-    pop0 (AtEnd n) = There0 (AtEnd (W1 n))
-
-instance SBinPI b => Pop0 ('B1 ('B0 b)) ('B1 b) where
-    pop0 (Here v)            = There0 (Here (W1 v))
-    pop0 (There1 (There0 p)) = There0 (There1 p)
-
-instance (SBinPI b, Pop0 a b) => Pop0 ('B1 a) ('B0 b) where
-    pop0 (There1 p) = There0 (pop0 p)
-    pop0 (Here v)   = There0 (weakenRight1V (W1 v))
-
-weakenRight1V :: forall b n. SBinPI b => Wrd ('S n) -> PosP' ('S n) b
-weakenRight1V v = case sbinp :: SBinP b of
-    SBE -> AtEnd v
-    SB0 -> There0 (weakenRight1V (W0 v))
-    SB1 -> Here v
+pop :: (SBinPI a, Pred b ~ 'BP a, BP.Succ a ~ b) => Pos ('BP a) -> Pos ('BP b)
+pop = weakenRight1
 
 -------------------------------------------------------------------------------
 -- Append and Split
